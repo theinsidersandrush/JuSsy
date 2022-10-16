db72b0e782c227e5d4d355c8ea923b905186f19b
The Insider Sandrush <itskomikabuseman@gmail.com>
2022-10-16 20:44:06 +0800

    new file:   README.md 	modified:   js/script.js

---
+++ README.md
@@ -0,0 +1,8 @@
+# JuSsy, the bussiest JavaScript-builded website
+    JuSsy, as the name suggests, is a project made by The Insider Sandrush for fun purposes.
+
+# License
+    This code is open-source and anyone can use it. However, you always need to link the [original source itself](https://github.com/theinsidersandrush/JuSsy).
+
+# Setup
+    Just clone it to your desired folder. Simple as that.
--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 console.log("%c"+"/* This is The Insider Sandrush's website. */","color: #0096fa; font-weight: bold;");
     console.log("We're currently operating a website for testing purposes. \nIf you find a vulnerability or bugs in the code, please report it to us.");
-    console.log("https://github.com/theinsidersandrush");
+    console.log("https://github.com/theinsidersandrush/JuSsy/issues");
